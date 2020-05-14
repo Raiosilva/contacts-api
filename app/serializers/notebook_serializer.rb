74eb8191a8395723d32dd4248b5bfe38258ec884
Raiosilva <raimundopi025@gmail.com>
2020-05-14 12:54:09 -0300

    adding associates

--- app/serializers/notebook_serializer.rb
+++ app/serializers/notebook_serializer.rb
@@ -1,6 +1,10 @@
 class NotebookSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_ano :address
+
   def attributes(*args)
     h = super(*args)
     # pt-BR -> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
